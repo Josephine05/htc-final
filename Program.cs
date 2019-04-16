a38cb796a806dc3869be7a4a087ac9d2c3dd2941
User <User@DEV-022827.OnBase.net>
2019-04-16 17:59:25 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace htc_final_project_Cehelnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
